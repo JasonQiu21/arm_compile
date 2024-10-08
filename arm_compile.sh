da8066245071478d9b636c164d89ca911c4fbf94
Jason Qiu <jqiu21@protonmail.com>
2024-10-08 20:53:19 +0000

    Update arm_compile.sh

--- arm_compile.sh
+++ arm_compile.sh
@@ -1,29 +1,24 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-usage(){
-    echo "Invalid usage. Type ./arm_compile.sh -h for help."
+show_help() {
+    echo "assemble, link, execute, and debug ARM assembly"
+    echo "author:  Ryan Monaghan, Jason Qiu"
+    echo "version: 1.0 - 10/4/2024"
+    echo
+    echo "Usage: asmExec [-h|-e|-d|-l|-g|-p <number>] -f <targetfile>"
+    echo "e    execute after linking"
+    echo "d    debug the program with gdb"
+    echo "p    port number for debugging (default: 4321)"
+    echo "l    link C library when assembling"
+    echo "g    assemble with debug information"
+    echo "o    output executable name"
+    echo "f    specify file to run"
 }
-
-help(){
-    echo "Script to assemble, link, execute, and debug ARM assembly"
-    echo
-    echo "Usage: ./arm_compile.sh [-h|-e|-l|-g|-o <file>] -f <targetfile>"
-    echo "h    Print this help"
-    echo "e    Execute after linking"
-    echo "l    Link C library when compiling"
-    echo "g    Debug with gdb (You will need to open another terminal to run gdb)"
-    echo "o    Specify output file (default: same as target)"
-    echo "f    Specify target file"
-    echo
-    echo "Script will leave assembled .o files as well as ARM binary, all with the same name as targetfile (NOT a.out) unless -o specified."
-    echo
-}
-
 
 # No options
 if [ $# -eq 0 ]
-then 
-    usage
+then
+    show_help
     exit
 fi
 
@@ -33,35 +28,38 @@ lc=""
 execute=0
 inputfile=""
 targetfile=""
-while getopts ":hgleo:f:" option; do
+port_num="4321"
+while getopts ":gledp:o:f:" option; do
     case $option in
-        h)
-            help
-            exit;;
+        l)
+            lc="-lc";;
+        g)
+            gdb="-g";;
+        d)
+            execute=2;;
         e)
             execute=1;;
-        l)
-            lc=" -lc";;
-        g)
-            gdb="-g ";;
+        f)
+            inputfile="$OPTARG";;
+        p) 
+            port_num="$OPTARG";;
         o)
             targetfile="$OPTARG"
             if [ -z $targetfile ] || [ "$targetfile" == "-f" ] # If targetfile is empty (check if equal to -f becasue if -f is called after it then we get this)
             then
-                echo "You must specify an output file. Type ./arm_compile.sh -h for help."
+                echo "you must specify an output file. type ./asmExec -h for help."
                 exit
             fi;;
-        f)
-            inputfile="$OPTARG";;
         ?)
-            usage
+            show_help
             exit;;
     esac
 done
 
+
 if [ -z $inputfile ] # If inputfile is empty
 then
-    echo "You must specify an input file. Type ./arm_compile.sh -h for help."
+    echo "no input file specified"
     exit
 fi
 
@@ -69,32 +67,37 @@ if [ -z $targetfile ]
 then
     targetfile=${inputfile%.*}
 fi
-echo "Compiling $inputfile..."
-aarch64-linux-gnu-as $gdb$inputfile -o $targetfile.o
-aarch64-linux-gnu-ld $targetfile.o -o $targetfile$lc
-echo "Done compiling."
+echo "assembling and linking '$inputfile'..."
+aarch64-linux-gnu-as $gdb $inputfile -o $targetfile.o
+aarch64-linux-gnu-ld $targetfile.o -o $targetfile $lc
+echo "done."
 
 if [ $execute -eq 1 ]
 then
-    echo "Executing ..."
+    echo "executing..."
     if [ -n $lc ]
-        then
-            qemu-aarch64 -L /usr/aarch64-linux-gnu/ $targetfile
-        else
-            qemu-aarch64 $targetfile
+    then
+        qemu-aarch64 -L /usr/aarch64-linux-gnu/ $targetfile
+    else
+        qemu-aarch64 $targetfile
     fi
-    echo "Done executing."
-fi
-
-
-if [ -z $gdb]
+    echo "done."
+elif [ $execute -eq 2 ]
 then
+    echo "executing in debug mode..."
+    if [ -n $lc ] 
+    then
+        qemu-aarch64 -L /usr/aarch64-linux-gnu/ -g $port_num $targetfile &
+    else
+        qemu-aarch64 -g $port_num $targetfile &
+    fi 
+    echo "starting debug on port $port_num..." &
+    gdb-multiarch --nh -q $targetfile \
+        -ex "set disassemble-next-line on" \
+        -ex "target remote :$port_num" \
+        -ex "set solib-search-path/usr/aarch64-linux-gnu-lib/" \
+        -ex "layout regs"
+    pkill -9 qemu
+    echo "done."
     exit
 fi
-qemu-aarch64 -L /usr/aarch64-linux-gnu/ -g 4321 $targetfile &
-echo "Starting debug on port 4321..." &
-gdb-multiarch --nh -q $targetfile \
--ex 'set disassemble-next-line on' \
--ex 'target remote :4321' \
--ex 'set solib-search-path/usr/aarch64-linux-gnu-lib/' \
--ex 'layout regs'
